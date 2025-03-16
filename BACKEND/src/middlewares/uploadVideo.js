1829961f4c6018b025abc4e00d869f07acdbd63a
ptquanh <vocsiquocanh@gmail.com>
2025-03-16 12:45:57 +0700

    add favicon.ico

--- BACKEND/src/middlewares/uploadVideo.js
+++ BACKEND/src/middlewares/uploadVideo.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../../config/cloudinary");
+const cloudinary = require("../config/cloudinary");
 
 // Cấu hình lưu video vào Cloudinary
 const storage = new CloudinaryStorage({
