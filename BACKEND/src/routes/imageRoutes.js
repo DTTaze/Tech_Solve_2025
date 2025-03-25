a685021967a8c6c61b6cf10dc364103b35838faa
Potato12925 <bao0908235279@gmail.com>
2025-03-25 23:51:38 +0700

    task submit, task approve

--- BACKEND/src/routes/imageRoutes.js
+++ BACKEND/src/routes/imageRoutes.js
@@ -7,7 +7,12 @@ const router = express.Router();
 router.post(
   "/upload",
   middlewareImage.single("image"),
-  imageController.uploadImage
+  imageController.handleUploadImage
 );
+
+router.get("/getall", imageController.handleGetAllImages);
+router.get("/get/:id", imageController.handleGetImageById);
+router.put("/update/:id", middlewareImage.single("image"), imageController.handleUpdateImage);
+router.delete("/delete/:id",middlewareImage.single("image"), imageController.handleDeleteImage);
 
 export default router;
