df47f9f198c9ee63d6c185a30b1703474e89b844
Potato12925 <bao0908235279@gmail.com>
2025-03-31 12:01:54 +0700

    fix get history of transaction by id, tasksubmit remove user_id

--- BACKEND/src/routes/taskRoutes.js
+++ BACKEND/src/routes/taskRoutes.js
@@ -14,6 +14,6 @@ router.post("/accept/:UserID/:id", taskController.handleAcceptTask);
 router.post("/complete/:id", taskController.handleCompleteTask);
 
 router.post("/submit/:task_user_id",middlewareImage.single("image"), taskController.handleSubmitTask);
-router.put("/submit/approve/:id", taskController.handleUpdateTaskSubmit);
+router.put("/submit/decision/:id", taskController.handleDecisionTaskSubmit);
 router.post("/coin/receive", taskController.handleReceiveCoin);
 export default router;
