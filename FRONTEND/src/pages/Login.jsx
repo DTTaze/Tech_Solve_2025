0912ee96c2ad8d1ceea3b718991278ccec9128d5
DTTaze <90083608+DTTaze@users.noreply.github.com>
2025-04-18 09:18:08 +0700

    Update Login.jsx

--- FRONTEND/src/pages/Login.jsx
+++ FRONTEND/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { loginUserApi } from "../utils/api";
 import { AuthContext } from "../contexts/auth.context";
 import InputField from "../components/ui/InputField";
 import Button from "../components/ui/Button";
+import SocialLoginIcons from "../components/ui/SocialLoginIcons";
 import { useNotification } from "../components/ui/NotificationProvider"; 
 
 const LoginPage = () => {
@@ -45,6 +46,7 @@ const LoginPage = () => {
             username: res.data.user?.username ?? "",
           },
         });
+        console.log(AuthContext.user);
         navigate("/")
       } else {
         notify("error", res.error || "Đăng nhập thất bại, vui lòng thử lại."); 
@@ -79,7 +81,7 @@ const LoginPage = () => {
       </form>
 
       <hr className="my-6 border-gray-300" />
-
+      <SocialLoginIcons></SocialLoginIcons>
       <div className="text-center">
         Chưa có tài khoản?{" "}
         <Link to="/register" className="text-blue-600 hover:underline">
