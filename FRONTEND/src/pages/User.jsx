e7b88251ea2967f84c4ad0e6aac8b7061a14dfc7
DTTaze <90083608+DTTaze@users.noreply.github.com>
2025-05-03 10:13:08 +0700

    skeleton loading

--- FRONTEND/src/pages/User.jsx
+++ FRONTEND/src/pages/User.jsx
@@ -3,11 +3,18 @@ import ProfileCard from "../components/features/user/ProfileCard.jsx";
 import PersonalInfoForm from "../components/features/user/PersonalInformation.jsx";
 import CompletedTaskList from "../components/features/user/CompletedTasksList.jsx";
 import TransactionHistory from "../components/features/user/TransactionHistory.jsx";
+import ProfileCardSkeleton from "../components/features/user/ProfileCardSkeleton.jsx";
+import PersonalInfoFormSkeleton from "../components/features/user/PersonalnfomationSkeleton.jsx";
 
 function UserProfile() {
   const [selectedTab, setSelectedTab] = useState("Thông tin cá nhân");
+  const [isLoading, setIsLoading] = useState(true);
 
   const renderContent = () => {
+    if (isLoading) {
+      return <PersonalInfoFormSkeleton />;
+    }
+
     switch (selectedTab) {
       case "Thông tin cá nhân":
         return <PersonalInfoForm />;
@@ -21,16 +28,14 @@ function UserProfile() {
   };
 
   return (
-    <>
-      <div className="w-screen min-h-screen bg-[#f7f8fa]">
-        <div className="w-[80vw] m-auto flex gap-3">
-          <div className="w-[30%] mt-4">
-            <ProfileCard setSelectedTab={setSelectedTab} />
-          </div>
-          <div className="w-[70%] mt-4">{renderContent()}</div>
+    <div className="w-screen min-h-screen bg-[#f7f8fa]">
+      <div className="w-[80vw] m-auto flex gap-3">
+        <div className="w-[30%] mt-4">
+          {isLoading ? <ProfileCardSkeleton /> : <ProfileCard setSelectedTab={setSelectedTab} />}
         </div>
+        <div className="w-[70%] mt-4">{renderContent()}</div>
       </div>
-    </>
+    </div>
   );
 }
 
